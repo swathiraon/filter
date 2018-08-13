8f2d2ac2ef1d05978af3ce8d96105b2029f8888a
vivek chandra <vivek.dexter.301096@gmail.com>
2018-08-13 19:02:12 +0530

    the front end is complete
    im not able to open the image of by selecting the path of the image.Its
    displaying the path but the image is not to be seen and its showing no
    error.

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+from PIL import Image
+from PIL import ImageFilter
+
+def rotate(i):
+	im=Image.open(i)
+	out=im.transpose(Image.ROTATE_90)
+	return out
+def bw(i):
+	im=Image.open(i)
+	bw1=im.convert('L')
+	return bw1 
+def crop(i,length,width):
+	size=(length,width)
+	im =Image.open(i)
+	im.thumbnail(size)
+	return im
+def blur(i):
+	im=Image.open(i)
+	blurred = im.filter(ImageFilter.BLUR)
+	blurred = blurred.filter(ImageFilter.BLUR)
+	return blurred
+def contrast(i,factor):
+	im=Image.open(i)
+	c = ImageEnhance.Contrast(im)
+	c = c.enhance(factor)
+	return c
--- filter.py
+++ filter.py
@@ -35,85 +35,191 @@
 
 
 from tkinter import * 
-from tkinter import filedialog
+from tkinter.filedialog import *
+import app
+from PIL import Image
 x=None
+open_file=None
+save_file=None
+
 root = Tk()
+history=[]
+# f1 = Frame(root,bd=2, width = 500, height =500)
+# f1.pack(side=LEFT, expand = 1)
+
+def main_image(path):
+	print(type(path))
+	print(type(canvas))
+	canvas.create_image(150,150, image=path)
+
+def preview_image(path):
+
+	      
+	canvas1.create_image(150,150, image=path)
+
+##########################opening and saving a file########################
+def open_image(): 
+	global open_file
+	open_file= askopenfilename() 
+	print(open_file)
+	#im=Image.open(open_file)
+	history.append(open_file)
+	img1 = PhotoImage(file=open_file)
+	main_image(img1)
+
+
+def close_image():
+	global save_file
+	save_file= askdirectory()
+	k=open_file.split("/")
+	p=k[-1]
+
+	os.rename(open_file,save_file+"/"+"p.jpg")
+
 
 
 f3 = Frame(root, bg = "black",bd=2, width = 10, height =7)
 f3.pack(side=TOP)
-B1 = Button(f3, text="Brown", fg="brown",width=80)
-B1.pack()
+Select = Button(f3, text="Select a Image", fg="brown",command=open_image,width=80)
+Select.pack()
 
 f3 = Frame(root, bg = "black",bd=2, width = 10, height =7)
 f3.pack(side=BOTTOM)
-B1 = Button(f3, text="Brown", fg="brown",width=80)
-B1.pack()
+close = Button(f3, text="Save", fg="brown",width=80,command=close_image)
+close.pack()
+#######################opening and saving a file#####################
 
-f = Frame(root ,bd=5, bg="BLACK",width = 1, height =1080)
+
+
+f = Frame(root ,bd=5,width = 1, height =1080)
 f.pack(side=LEFT)
 
 def pix():
 	s=pixels.get()
 	print(s)
-
-B1 = Button(f, text="Brown", fg="brown")
-B1.grid(column=2,row=0,padx=10, pady=100)
-B1 = Button(f, text="Brown", fg="brown")
-B1.grid(column=2,row=5,padx=10, pady=100)
-B1 = Button(f, text="Brown", fg="brown")
-B1.grid(column=2,row=10,padx=10, pady=10)
-B1 = Button(f, text="Brown", fg="brown")
+#rotating the image
+def call_rotate():
+	path=history[-1]
+	im=rotate(path)
+	history.append(im)
+	main_image(im)
 
 
-B1.grid(column=3,row=20, pady=10)
+Rotate = Button(f, text="Rotate", fg="brown",command=call_rotate)
+Rotate.grid(column=2,row=0,padx=10, pady=50)
+
+#bluring the image
+def call_blur():
+	path=history[-1]
+	im=blur(path)
+	history.append(im)
+	main_image(im)
+
+blur = Button(f, text="Blur", fg="brown",command=call_blur)
+blur.grid(column=2,row=5,padx=10, pady=50)
+
+#change to black and white
+def call_b2w():
+	path=history[-1]
+	im=blur(path)
+	history.append(im)
+	main_image(im)
+b2w = Button(f, text=" B&W ", fg="brown",command=call_b2w)
+b2w.grid(column=2,row=10,padx=10, pady=50)
+
+#corp the image
+def accept():
+	length=pixels.get()
+	width=pixel.get()
+	path=history[-1]
+	im=crop(path,length,width)
+	history.append(im)
+	main_image(im)
+
 pixels=Entry(f, width = 10)
-pixels.focus_set()
-s=pixels.get()
-print(s)
 pixels.grid(column=0,row=20)	
+
 Label(f, text = "x").grid(column = 1, row = 20) 
-pixel=Entry(f, width = 10, textvariable = x)
+pixel=Entry(f, width = 10)
 pixel.grid(column=2,row=20)
 
 
-f1 = Frame(root, bg = "BLACK",bd=2, width = 500, height =500)
-f1.pack(side=LEFT, expand = 1)
+crop = Button(f, text="Crop", fg="brown",command=accept)
+crop.grid(column=3,row=20, pady=10)
 
+
+############ original image of selected by the user#############
+f1 = Frame(root,bd=2, width = 500, height =500)
+f1.pack(side=LEFT, expand = 1)
 Label(f1, text = "YOUR image").pack()
 Label(f1).pack()
-canvas = Canvas(f1, width = 300, height = 300)      
+
+canvas = Canvas(f1, width = 300, height = 300)
+
 canvas.pack()
-img1 = PhotoImage(file="A8R.gif")      
-canvas.create_image(150,150, image=img1)
 
-
-
-f11 = Frame(root, bg = "GREEN",bd=2, width = 500, height =500)
+##############preview of the image and the filter####################
+f11 = Frame(root,bd=2, width = 500, height =500)
 f11.pack(side=LEFT,expand=1)
 Label(f11, text = "preview").pack()
 Label(f11).pack()
 canvas1 = Canvas(f11, width = 300, height = 300)      
-canvas1.pack()    
-img = PhotoImage(file="A8R.gif")      
-canvas1.create_image(150,150, image=img)
+canvas1.pack()
 
-f2 = Frame(root, bg = "black",bd=2, width = 500, height =500)
+
+
+
+
+
+###############the filter side part###############33333
+f2 = Frame(root,bd=2, width = 500, height =500)
 f2.pack(side=RIGHT)
+###########the filter button#########33333333
+cool = Button(f2, text="Cool", fg="brown")
+cool.grid(column=3,row=0,padx=10, pady=30)
+pop = Button(f2, text="PoP", fg="brown")
+pop.grid(column=3,row=5,padx=10, pady=30)
+chrome = Button(f2, text="Chrome", fg="brown")
+chrome.grid(column=3,row=10,padx=10, pady=30)
+film = Button(f2, text="Film", fg="brown")
+film.grid(column=3,row=20, pady=10,padx=30)
 
-B1 = Button(f2, text="Brown", fg="brown")
-B1.grid(column=2,row=0,padx=10, pady=100)
-B1 = Button(f2, text="Brown", fg="brown")
-B1.grid(column=2,row=5,padx=10, pady=100)
-B1 = Button(f2, text="Brown", fg="brown")
-B1.grid(column=2,row=10,padx=10, pady=10)
-B1 = Button(f2, text="Brown", fg="brown")
-B1.grid(column=3,row=20, pady=10,padx=40)
+############the brightness and contrast sharpness and the color##########
 
 
+	
+
+Label(f2, text = "Brightness").grid(column=2,row=29)
+var = DoubleVar()
+scale1 = Scale( f2, variable = var )
+scale1.grid(column=2, row=30)
+
+def cont():
+	k=var1.get()
+	path=history[-1]
+	im=contrast(path,k)
+	history.append(im)
+	main_image(im)
+
+Label(f2, text = "Contrast").grid(column=4,row=29)
+var1= DoubleVar()
+scale2 = Scale( f2, variable = var1)
+scale2.grid(column=4, row=30)
 
 
+Label(f2, text = "Sharpness",pady=5).grid(column=2,row=59)
+var2= DoubleVar()
+scale3 = Scale( f2, variable = var2 )
+scale3.grid(column=2, row=60)
 
+Label(f2, text = " Color " ,pady=5).grid(column=4,row=59)
+var2= DoubleVar()
+scale3 = Scale( f2, variable = var2 )
+scale3.grid(column=4, row=60)
+
+
+undo = Button(f2, text="Undo", fg="brown",bg="blue")
+undo.grid(column=3,row=90, pady=50,padx=30)
 
 
 # f3 = Frame(f, bg = "red", width = 500)
